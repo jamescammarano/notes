af41ce8e11f1eb22e2968d4d55c28a1efbe11fab
jamescammarano <jay.cammarano@gmail.com>
2023-03-04 21:49:58 -0500

    - cards - new routine button

--- src/pages/routines.tsx
+++ src/pages/routines.tsx
@@ -5,6 +5,93 @@ import Link from "next/link";
 import { RoutineContext } from "../context/Routine.context";
 import { useContext } from "react";
 import Image from "next/image";
+import { api } from "../utils/api";
+import { unsavedRoutineSchema } from "../schemas/todo";
+
+type Card = {
+  id: string;
+  title: string;
+  image: string;
+  inverted_color: string;
+};
+
+type Cards = {
+  data: Card[];
+};
+
+const DEFAULT_PLACEHOLDER =
+  "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=480&h=480&q=80";
+
+const Card: React.FC<Card> = ({ id, title, image, inverted_color }) => {
+  return (
+    <>
+      <div className="w-full rounded-md border-[1px] border-t-4 border-primary pt-1 shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+        <Link href={`/routines/${id}`}>
+          <div className="bg-secondary text-inverted-100 flex flex-col overflow-hidden rounded-md p-3 sm:p-6">
+            <div className="justify-center">
+              <h2 className="truncate text-center text-lg sm:text-lg">
+                {title}
+              </h2>
+            </div>
+            <div className="pt-4">
+              <Image
+                src={image}
+                style={{ background: inverted_color }}
+                alt={title}
+                width={240}
+                height={240}
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export const Cards: React.FC<Cards> = ({ data }) => {
+  const { refetch } = useContext(RoutineContext);
+
+  const { mutateAsync } = api.todo.createRoutine.useMutation();
+
+  const handleClick = async () => {
+    const parseResults = unsavedRoutineSchema.safeParse({
+      title: `My Routine #${data.length + 1}`,
+    });
+
+    if (parseResults.success) {
+      try {
+        await mutateAsync({ title: parseResults.data.title });
+        await refetch();
+      } catch (error) {
+        //  TODO Error handling
+      }
+    }
+  };
+  return (
+    <div className="pb-12">
+      <div className="mt-1 grid grid-cols-2 gap-8 md:grid-cols-4">
+        <div className="splash col-span-2 rounded-md">
+          <button
+            onClick={() => void handleClick()}
+            className="mx-auto flex h-full flex-row items-center lg:text-[3rem]"
+          >
+            New Routine
+          </button>
+        </div>
+        {data.map((contents) => (
+          <Card
+            inverted_color={contents.inverted_color}
+            key={contents.id}
+            id={contents.id}
+            title={contents.title}
+            image={contents.image ?? DEFAULT_PLACEHOLDER}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Routine: NextPage = () => {
   const { routines } = useContext(RoutineContext);
@@ -14,32 +101,13 @@ const Routine: NextPage = () => {
       <Head>
         <title>Nightlite: Bedtime Routines</title>
       </Head>
-      <main className="flex min-h-screen flex-col bg-background-400 text-inverted">
+      <main className="flex min-h-screen min-w-full flex-col bg-background-400 text-inverted">
         <div className="flex">
           <Sidebar />
-          <div className="w-4/5">
-            <div className="flex flex-wrap">
-              {routines &&
-                routines.map((routine) => {
-                  return (
-                    <div
-                      className="m-2 w-52 rounded bg-gray-600 bg-opacity-70 p-3 text-center"
-                      key={routine.id}
-                    >
-                      <Link href={`/routines/${routine.id}`}>
-                        <Image
-                          className="mb-2 rounded border-4 border-white bg-opacity-70"
-                          style={{ background: routine.inverted_color }}
-                          alt={routine.title}
-                          width={240}
-                          height={240}
-                          src={routine.image}
-                        />
-                        {routine.title}
-                      </Link>
-                    </div>
-                  );
-                })}
+          <div className="w-full px-8">
+            <h1 className="mt-12 mb-8 text-4xl">Your Routines</h1>
+            <div className="w-full">
+              {routines && <Cards data={routines} />}
             </div>
           </div>
         </div>
